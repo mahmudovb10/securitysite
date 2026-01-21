6b4ec3d9c14bbdceb0e742f43b4f6d1193459688
Bahodirxon <bohodirxon.mahmudoov@gmail.com>
2026-01-21 23:35:56 +0500

    fixing

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: ["https://securitysite-eight.vercel.app", "http://localhost:5173"],
     credentials: true,
   }),
 );
