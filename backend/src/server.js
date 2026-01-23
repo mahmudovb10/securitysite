2a48fe0f78e26c1863de9d70c00fc37ba61c8fd2
Bahodirxon <bohodirxon.mahmudoov@gmail.com>
2026-01-23 13:30:05 +0500

    update serverr

--- backend/src/server.js
+++ backend/src/server.js
@@ -37,7 +37,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // 4. DATABASE CONNECTION
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ MongoDB Connected");
   })
